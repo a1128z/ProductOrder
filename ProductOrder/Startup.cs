99827a07cedf7c0f6a79634fb4f2f799ad2602e6
a1128z <a1128z@hotmail.com>
2017-06-14 20:54:24 +0800

    Add ProductOrder project

---
+++ ProductOrder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProductOrder.Startup))]
+namespace ProductOrder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
